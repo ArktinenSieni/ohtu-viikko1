a50a3fb9ce1b25f8f36104c0a457e6e9ae99a41f
Räty Matti J <mcraty@da3-cs-dk110-10.helsinki.fi>
2014-07-28 17:21:02 +0300

    varmistus

--- DiscoTetris/src/main/java/sienimetsa/discotetris/logiikka/lauta.java
+++ DiscoTetris/src/main/java/sienimetsa/discotetris/logiikka/lauta.java
@@ -1,15 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sienimetsa.discotetris.logiikka;
 
-/**
- *
- * @author mcraty
- */
 public class lauta {
+    private palikka[][] areena;
+    
+    public lauta() {
+        areena = new palikka[6][12];
+    }
+    
+    public void tyhjenna() {
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 6; j++) {
+                this.areena[i][j].setVari(0);
+            }
+        }
+    }
+    
     
 }
--- DiscoTetris/src/main/java/sienimetsa/discotetris/logiikka/palikka.java
+++ DiscoTetris/src/main/java/sienimetsa/discotetris/logiikka/palikka.java
@@ -1,7 +1,46 @@
 package sienimetsa.discotetris.logiikka;
 
+import java.util.Random;
+
 public class palikka {
-    private sijainti;
-    private vari;
+    private int vari;
+    private int pysty;
+    private int vaaka;
     
+    public palikka(int vari, int pysty, int vaaka) {
+        this.vari = vari;
+        this.pysty = pysty;
+        this.vaaka = vaaka;
+    }
+
+    public int getVari() {
+        return vari;
+    }
+    
+    public void setVari(int parametri) {
+        this.vari = vari;
+    }
+
+    public int getPysty() {
+        return pysty;
+    }
+
+    public int getVaaka() {
+        return vaaka;
+    }
+    
+    public void arvoVari() {
+        Random arpoja = new Random();
+        
+        while (this.vari == 0) {
+            setVari(arpoja.nextInt(6));
+        }
+    }
+    
+    public void vaihdaVareja(palikka vaihdettava) {
+        int apu = this.vari;
+        
+        setVari(vaihdettava.getVari());
+        vaihdettava.setVari(apu);
+    }
 }
